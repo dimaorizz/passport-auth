1674c2c8eb1ed3b6b0b2b5cf071bf859f37ce161
Dmitriy <dimaorizz@gmail.com>
2020-01-25 20:16:25 +0200

    added custom handler for auth

--- routes/login.js
+++ routes/login.js
@@ -6,10 +6,16 @@ router.get('/', (req, res) => {
     res.render('login')
 })
 
-router.post('/', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
+router.post('/', function(req, res, next) {
+    passport.authenticate('local', function(err, user) {
+      if (err) { return next(err); }
+      if (!user) { return res.redirect('/login'); }
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        console.log(user)
+        return res.redirect('/')
+      });
+    })(req, res, next);
+  });
 
 module.exports = router
